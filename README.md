02df1a6c0426bb7b3b66716612156999a6c7d173
Dominic McGinty <McGinty.Dom@gmail.com>
2022-12-03 16:03:03 +0000

    SP-1: Project Setup

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # story-pointer
+## starting the application
+## development process
+npm run test
+npm run lint-fix
+git push origin [branch-name]
---
+++ src/components/Card/Card.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Card from "./Card";
+
+export default {
+    component: Card,
+    title: "Components/Card"
+}
+
+export const Tester = () => {
+    return <Card></Card>;
+}
